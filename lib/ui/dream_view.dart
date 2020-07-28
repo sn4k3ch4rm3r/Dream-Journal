630fa85c59c6f74ca3c8dc77f9a2535a454f9e7d
Boldizsár Tóth <bocko200@gmail.com>
2020-07-28 13:42:22 +0200

    Backpress works with backbutton not just the in app back

--- lib/ui/dream_view.dart
+++ lib/ui/dream_view.dart
@@ -38,131 +38,138 @@ class _DreamViewState extends State<DreamView> {
         date: DateTime.now(),
       );
     }
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.isNew ? 
-          'New Dream' :
-          widget.edit ?
-          'Edit Dream' :
-          'Dream'
-        ),
-        leading: BackButton(
-          onPressed: () {
-            if(edited) {
-              Navigator.pop(context, 'edited');
-            }
-            else {
-              Navigator.pop(context);
-            }
-          },
-        ),
-        actions: dream != null && !widget.edit ? 
-          <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.edit,
-                color: Theme.of(context).primaryIconTheme.color,
-              ),
-              onPressed: () async {
-                Dream result = await Navigator.push(context, MaterialPageRoute(builder: (context)=>DreamView(dream: dream, edit:true,)));
-                if(result != null) {
-                  setState(() {
-                    edited = true;
-                    dream ??= result;
-                  });
-                }
-              },
-            ),
-            IconButton(
-              icon: Icon(
-                Icons.delete,
-                color: Theme.of(context).primaryIconTheme.color,
-              ),
-              onPressed: () {
-                Dialogs.deleteConfirmDialog(context).then((confirmed) {
-                  if(confirmed) {
-                    DatabaseProvider.db.delete(dream);
-                    Navigator.pop(context, 'deleted');
-                  }
-                });
-              },
-            ),
-          ]
-          : null,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              RaisedButton(
-                onPressed:() {
-                  if(widget.edit) 
-                    showDatePicker(
-                      context: context,
-                      initialDate: dream.date,
-                      firstDate: DateTime.fromMillisecondsSinceEpoch(0),
-                      lastDate: DateTime.now()
-                    ).then((value) => {
-                      if(value != null)
-                        setState((){
-                          dream.date = value;
-                        })
+    return WillPopScope(
+      onWillPop: () async {
+        if(edited)
+          Navigator.pop(context, 'edited');
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            widget.isNew ? 
+            'New Dream' :
+            widget.edit ?
+            'Edit Dream' :
+            'Dream'
+          ),
+          leading: BackButton(
+            onPressed: () {
+              if(edited) {
+                Navigator.pop(context, 'edited');
+              }
+              else {
+                Navigator.pop(context);
+              }
+            },
+          ),
+          actions: dream != null && !widget.edit ? 
+            <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.edit,
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+                onPressed: () async {
+                  Dream result = await Navigator.push(context, MaterialPageRoute(builder: (context)=>DreamView(dream: dream, edit:true,)));
+                  if(result != null) {
+                    setState(() {
+                      edited = true;
+                      dream ??= result;
                     });
+                  }
                 },
-                child: Text(
-                  '${dateFormat.format(dream.date)}',
-                  style: Theme.of(context).textTheme.headline6,
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.delete,
+                  color: Theme.of(context).primaryIconTheme.color,
                 ),
+                onPressed: () {
+                  Dialogs.deleteConfirmDialog(context).then((confirmed) {
+                    if(confirmed) {
+                      DatabaseProvider.db.delete(dream);
+                      Navigator.pop(context, 'deleted');
+                    }
+                  });
+                },
               ),
-              TextField(
-                controller: _controller,
-                enabled: widget.edit,
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                textInputAction: TextInputAction.done,
-                style: Theme.of(context).textTheme.bodyText2,
-                decoration: InputDecoration(
-                  fillColor: Theme.of(context).primaryColorLight,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0)
+            ]
+            : null,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                RaisedButton(
+                  onPressed:() {
+                    if(widget.edit) 
+                      showDatePicker(
+                        context: context,
+                        initialDate: dream.date,
+                        firstDate: DateTime.fromMillisecondsSinceEpoch(0),
+                        lastDate: DateTime.now()
+                      ).then((value) => {
+                        if(value != null)
+                          setState((){
+                            dream.date = value;
+                          })
+                      });
+                  },
+                  child: Text(
+                    '${dateFormat.format(dream.date)}',
+                    style: Theme.of(context).textTheme.headline6,
                   ),
-                  hintText: 'Describe your dream in as much detail as you can'
                 ),
-                cursorColor: Theme.of(context).accentColor,
-              ),
-              Row(
-                children: <Widget>[
-                  Text('Lucidity:'),
-                  Switch(
-                    value: dream.isLucid,
-                    onChanged: (value) {
-                      if(widget.edit)
-                        setState(() {
-                          dream.isLucid = value;
-                        });
-                    },                  
+                TextField(
+                  controller: _controller,
+                  enabled: widget.edit,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  textInputAction: TextInputAction.done,
+                  style: Theme.of(context).textTheme.bodyText2,
+                  decoration: InputDecoration(
+                    fillColor: Theme.of(context).primaryColorLight,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0)
+                    ),
+                    hintText: 'Describe your dream in as much detail as you can'
                   ),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Text('Vividity:'),
-                  Slider(
-                    value: dream.vividity/100,
-                    onChanged: (value){
-                      if(widget.edit)
-                        setState(() {
-                          dream.vividity = (value*100).floor();
-                        });
-                    },
-                  ),
-                ],
-              ),
-              getSaveButton(),
-            ],
+                  cursorColor: Theme.of(context).accentColor,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text('Lucidity:'),
+                    Switch(
+                      value: dream.isLucid,
+                      onChanged: (value) {
+                        if(widget.edit)
+                          setState(() {
+                            dream.isLucid = value;
+                          });
+                      },                  
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text('Vividity:'),
+                    Slider(
+                      value: dream.vividity/100,
+                      onChanged: (value){
+                        if(widget.edit)
+                          setState(() {
+                            dream.vividity = (value*100).floor();
+                          });
+                      },
+                    ),
+                  ],
+                ),
+                getSaveButton(),
+              ],
+            ),
           ),
         ),
       ),
